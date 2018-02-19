66529d4c5fdbf91468a53cb86357e572b151e733
mobajunior <mobajunior@outlook.com>
2018-02-19 19:06:42 -0400

    Update main.m

--- Descente de gradient/main.m
+++ Descente de gradient/main.m
@@ -1,28 +1,41 @@
-%main est le script qui va nous permetter de calculer l'hypithèse, les
+%Nom: Moussa Bâ
+
+%main est le script qui va nous permetter de calculer l'hypothèse, les
 %probabilité et la fonction de coût
 %variables d'entrée
+
 X=load('tp1x.dat');
 %variables de sortie
 Y=load('tp1y.dat');
+
 plot(X, Y, '.');
 ylabel('Taille');
+
 xlabel('Age');
 title('Taille en fonction de l''âge');
+
 temp0=0;
 temp1=0;
+
 %Taux d'apprentissage
 alpha=0.07;
+
 %nombre d'exemple d'entrainement
 m=length(X);
+
 %on rajoute à X une dimension pour calculer les theta avec une matrice
 X=[ones(m, 1), X];
+
 %Theta est une matrice qui définit Theta0 pour la 1ère rangée et Theta1
 %pour la deuxième rangée
 theta=zeros(2,1);
+
 %nombre d'itération pour Theta ave la descente de gradient
 iteration=1500;
+
 %Calcul de Theta avec la descente de gradient
 theta=Descente_gradient(theta, alpha, X, Y, iteration, m);
+
 %Affichage de Theta
 fprintf('\n Valeurs de Theta trouvées par descente de gradient');
 fprintf('\n Theta0 = ');
@@ -33,8 +46,10 @@ hold on
 plot(X(:, 2), X*theta, '-');
 legend('Données d''entrainement', 'Régression linéaire');
 hold off;
+
 %Prédisons les tailles des enfants selon l'âge donné dans le tableau
 fprintf('Prédiction des tailles en fonction de l''âge \n');
+
 %Tableu contenant âges des enfants dont on veut prédire leurs tailles
 tab=[3.5, 5, 5.5, 7];
 predict=ones(1,4);
@@ -52,6 +67,7 @@ hold on;
 plot(tab, predict, 'x');
 legend('Données d''entrainement', 'Régression linéaire', 'Prédictions des tailles');
 hold off;
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%Visualisation de la fonction de coût%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -73,6 +89,7 @@ surf(theta0_vals, theta1_vals, J_vals)
 xlabel('\theta_0');
 ylabel('\theta_1')
 figure;
+
 % Plot the cost function with 15 contours spaced logarithmically
 % between 0.01 and 100
 contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 2, 15))
